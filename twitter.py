acd0be30b2940508ea4e179d2f65fcb59828d76f
Louis Mitas <lmitas@outlook.com>
2019-06-14 15:22:17 -0400

    Formatting
    Improving readability with idiomatic python!

--- twitter.py
+++ twitter.py
@@ -4,35 +4,38 @@ import eventdb
 from pathlib import Path
 
 def parseRawTwitter(rawFileText):
-    openBracketPos = -1
-    closeBracketPos = -1
-    listOfTweets = []
+    
+    openBracketPos = closeBracketPos = -1
+    listOfTweets = list()
     
     while(rawFileText.find("{", closeBracketPos + 1) > -1):
         
         openBracketPos = rawFileText.find("{", closeBracketPos + 1)
         maybeClosePos = rawFileText.find("}\n}, {", openBracketPos + 1)
         
-        if(maybeClosePos == -1): closeBracketPos = rawFileText.find("}\n} ]", openBracketPos + 1) + 3
-        else: closeBracketPos = rawFileText.find("}\n}, {", openBracketPos + 1) + 3
+        searchString = "}\n} ]" if maybeClosePos == -1 else "}\n}, {"
         
+        closeBracketPos = rawFileText.find(searchString, openBracketPos + 1) + 3
+            
         singleTweetString = rawFileText[openBracketPos:closeBracketPos]
         
         tweetJson = json.loads(singleTweetString)
         
         rawTweetText = str(tweetJson["text"])
         convertedTweetText = bytes(rawTweetText,"latin1",errors="ignore").decode("latin1")
-        tweetJson["text"] = convertedTweetText        
+        tweetJson["text"] = convertedTweetText
         
         tweetTimeStamp = parseDateTime(tweetJson["created_at"])
         tweetJson["sqlDate"] = str(tweetTimeStamp.date())
         tweetJson["sqlTime"] = str(tweetTimeStamp.time())
         
         listOfTweets.append(tweetJson)
+        
     return listOfTweets
 
 def parseDateTime (rawStamp):
-    if(rawStamp[0:4].isnumeric() == True):
+    
+    if rawStamp[0:4].isnumeric():
         
         yr = int(rawStamp[0:4])
         mo = int(rawStamp[5:7])
@@ -41,7 +44,8 @@ def parseDateTime (rawStamp):
         mn = int(rawStamp[14:16])
         sc = int(rawStamp[17:19])
         
-    elif(rawStamp[0:3].isalpha() == True):
+    elif rawStamp[0:3].isalpha():
+        
         yr = int(rawStamp[26:30])
         mo = numberMonth(rawStamp[4:7])
         dy = int(rawStamp[8:10])
@@ -49,39 +53,76 @@ def parseDateTime (rawStamp):
         mn = int(rawStamp[14:16])
         sc = int(rawStamp[17:19])
         
-    elif(rawStamp.find(" - ") >= 0):
+    elif rawStamp.find(" - ") >= 0:
+        
         yr = int(rawStamp[len(rawStamp)-4:len(rawStamp)])
         
-    postDateTime = datetime.datetime(yr,mo,dy,hour = hr,minute = mn,second = sc)
+    return datetime.datetime(yr,mo,dy,hour = hr,minute = mn,second = sc)
     
-    return postDateTime
 
 def numberMonth(monthStr):
-    monthStr = monthStr.capitalize()
-    if(monthStr[0:3] == "Jan"): monthNum = 1
-    elif(monthStr[0:3] == "Feb"): monthNum = 2
-    elif(monthStr[0:3] == "Mar"): monthNum = 3
-    elif(monthStr[0:3] == "Apr"): monthNum = 4
-    elif(monthStr[0:3] == "May"): monthNum = 5
-    elif(monthStr[0:3] == "Jun"): monthNum = 6
-    elif(monthStr[0:3] == "Jul"): monthNum = 7
-    elif(monthStr[0:3] == "Aug"): monthNum = 8
-    elif(monthStr[0:3] == "Sep"): monthNum = 9
-    elif(monthStr[0:3] == "Oct"): monthNum = 10
-    elif(monthStr[0:3] == "Nov"): monthNum = 11
-    elif(monthStr[0:3] == "Dec"): monthNum = 12
     
-    return monthNum
+    monthStr = monthStr.capitalize()
+    
+    if(monthStr[0:3] == "Jan"):
+        
+        return 1
+    
+    elif(monthStr[0:3] == "Feb"):
+        
+        return 2
+    
+    elif(monthStr[0:3] == "Mar"):
+        
+        return 3
+    
+    elif(monthStr[0:3] == "Apr"):
+        
+        return 4
+    
+    elif(monthStr[0:3] == "May"):
+        
+        return 5
+    
+    elif(monthStr[0:3] == "Jun"):
+        
+        return 6
+    
+    elif(monthStr[0:3] == "Jul"):
+        
+        return 7
+    
+    elif(monthStr[0:3] == "Aug"):
+        
+        return 8
+    
+    elif(monthStr[0:3] == "Sep"):
+        
+        return 9
+    
+    elif(monthStr[0:3] == "Oct"):
+        
+        return 10
+    
+    elif(monthStr[0:3] == "Nov"):
+        
+        return 11
+    
+    elif(monthStr[0:3] == "Dec"):
+        
+        return 12
+    
 
 def processDirectory(dirPath):
     
     targetDir = Path(dirPath)
     
-    cnx = eventdb.createConnection()
+    cnx = eventdb.createConnection('test_one')
     
     for targetFile in targetDir.iterdir():
         
         with open(targetFile,"r",encoding="latin1",errors="replace") as file:
+            
             file = file.read()
             listOfTweets = parseRawTwitter(file)
             eventdb.insertTweets(listOfTweets,cnx)
@@ -96,7 +137,7 @@ def getOneTweet(tweetid):
     
     theTweet = eventdb.getTweet(cursor,tweetid)
     
-    output = []
+    output = list()
     
     for i in cursor:
         output.append(i)
@@ -105,6 +146,6 @@ def getOneTweet(tweetid):
     
     return output
 
-# processDirectory("data")
+processDirectory("data")
 
-print(getOneTweet('155316636524613633'))
+#print(getOneTweet('155316636524613633'))
