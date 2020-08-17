12fe46dddae084d7dbb45eee2e88c4b844e08f1a
milesbelli <lmitas@outlook.com>
2020-08-17 00:45:12 -0400

    Reverse Order flag not an int Needed to make a chance since reverse order flag is not stored in DB as an int, needed to set it to that in the __init__ logic.
    fixes #34

--- twitter.py
+++ twitter.py
@@ -596,8 +596,7 @@ class UserPreferences:
         self.user_id = user_id
         db_prefs = eventdb.get_user_preferences(self.user_id)
         self.timezone = db_prefs.get('timezone') or 'UTC'
-        self.reverse_order = db_prefs.get('reverse_order')
-        print(self.reverse_order)
+        self.reverse_order = int(db_prefs.get('reverse_order'))
 
     def update(self, **kwargs):
         self.timezone = kwargs.get('timezone') or self.timezone
