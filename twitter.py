655cdd30dd94f2eca7bcc30f66438ca389db0705
milesbelli <lmitas@outlook.com>
2020-08-23 11:21:46 -0400

    Reverse order flag Fixed issue where app would not work if reverse order flag didn't exist in db.

--- twitter.py
+++ twitter.py
@@ -596,7 +596,7 @@ class UserPreferences:
         self.user_id = user_id
         db_prefs = eventdb.get_user_preferences(self.user_id)
         self.timezone = db_prefs.get('timezone') or 'UTC'
-        self.reverse_order = int(db_prefs.get('reverse_order'))
+        self.reverse_order = int(db_prefs.get('reverse_order') or 0)
 
     def update(self, **kwargs):
         self.timezone = kwargs.get('timezone') or self.timezone
