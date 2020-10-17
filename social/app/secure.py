a27213ff1ec61ef446d44f780b95e7b22a871f01
milesbelli <lmitas@outlook.com>
2020-10-17 00:34:10 -0400

    Adding secure.py Given that these are all default values anyway, there is no reason for this not to be included in the distribution.

---
+++ social/app/secure.py
@@ -0,0 +1,13 @@
+# This is the user for the database
+def username():
+    return "socialuser"
+
+
+# This is the password for the database user
+def password():
+    return "resetme"
+
+
+# Define the address for the database
+def host():
+    return "127.0.0.1"
